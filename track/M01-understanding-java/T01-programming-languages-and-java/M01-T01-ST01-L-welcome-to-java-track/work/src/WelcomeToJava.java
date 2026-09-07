08426bed542952d67ff7e1a6f6bd6049e25ca625
Lokesh Telapolu <mslokeshtelapolu@gmail.com>
2026-09-07 23:25:02 +0530

    this is my update

--- track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/WelcomeToJava.java
+++ track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/WelcomeToJava.java
@@ -3,7 +3,6 @@ public class WelcomeToJava {
        //Write your code here
        System.out.println("Welcome to Java");
        System.out.println("I am a developer");
-       System.out.println("I am a software developer");
     }
 }
 
