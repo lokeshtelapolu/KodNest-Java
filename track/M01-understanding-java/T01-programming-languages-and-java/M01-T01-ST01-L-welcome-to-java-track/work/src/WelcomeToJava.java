1e2004a63c1f70b9fd34c0d1f7a966bffbd4e517
Lokesh Telapolu <mslokeshtelapolu@gmail.com>
2026-09-07 22:57:21 +0530

    this is my update

--- track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/WelcomeToJava.java
+++ track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/WelcomeToJava.java
@@ -2,5 +2,7 @@ public class WelcomeToJava {
     public static void main(String[] args) {
        //Write your code here
        System.out.println("Welcome to Java");
+       System.out.println("I am a developer");
     }
 }
+
